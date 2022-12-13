a5784fa0b70f0633d48f7aea41885a46d0be04f5
GalicaYlber <galica.ylber@gmail.com>
2022-12-13 15:48:56 +0200

    Snake Move
    Snake moves on its own but not on grid

--- Assets/MoveSnake.cs
+++ Assets/MoveSnake.cs
@@ -6,35 +6,54 @@ using UnityEngine.Tilemaps;
 public class MoveSnake : MonoBehaviour
 {
     public Tilemap tilemap;
+    public float snakeSpeed;
 
     private Vector3 snakePosition;
+    private string snakeDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         snakePosition = transform.position;
+        snakeDirection = "right";
+        snakeSpeed *= 0.001f;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.A)) {
-            transform.position += Vector3.left * 1f;
+            snakeDirection = "left";
         }
         if(Input.GetKeyDown(KeyCode.D)) {
-            transform.position += Vector3.right * 1f;
+            snakeDirection = "right";
         }
         if(Input.GetKeyDown(KeyCode.W)) {
-            transform.position += Vector3.up * 1f;
+            snakeDirection = "up";
         }
         if(Input.GetKeyDown(KeyCode.S)) {
-            transform.position += Vector3.down * 1f;
+            snakeDirection = "down";
         }
 
         if(Input.GetKey(KeyCode.Space)) {
             transform.position = tilemap.CellToWorld(new Vector3Int(0, 0, 0));
 
             Debug.Log(transform.position);
+        }
+
+        switch(snakeDirection) {
+            case "left":
+                transform.position += Vector3.left * snakeSpeed;
+                break;
+            case "right":
+                transform.position += Vector3.right * snakeSpeed;
+                break;
+            case "up":
+                transform.position += Vector3.up * snakeSpeed;
+                break;
+            case "down":
+                transform.position += Vector3.down * snakeSpeed;
+                break;
         }
     }
 
